84a32671d789929c533e8b90c4fdd613fac55abb
fouzaikhawlaf <fouzaikhawla123@gmail.com>
2024-07-31 19:51:06 +0100

    test12

--- CleanArchitecture.FrameworkAndDrivers/Data/Repository/SaleRepository.cs
+++ CleanArchitecture.FrameworkAndDrivers/Data/Repository/SaleRepository.cs
@@ -48,6 +48,7 @@ namespace CleanArchitecture.FrameworkAndDrivers.Data.Repository
                 await _context.SaveChangesAsync();
             }
             return sale;
+            //bonjour
         }
     }
     
